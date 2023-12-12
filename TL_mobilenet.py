ecec8feb107083a1cb77997e71daf576c4ce77fc
Wahyu Luna Wijaya <142510806+WahyuLuna@users.noreply.github.com>
2023-12-12 22:43:10 +0900

    Add files via upload

---
+++ TL_mobilenet.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+
+# Define path to the dataset
+base_dir = "C:\VS CODE FILE FACTORY\Bangkit Product\more chicken\Train_80,20"
+train_dir = base_dir + "/train"
+val_dir = base_dir + "/validation"
+image_size = (224, 224)
+batch_size = 32
+
+# Use ImageDataGenerator for data augmentation
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+# Create data generators
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=image_size,
+    batch_size=batch_size,
+    class_mode='categorical',
+    classes=['healthy', 'ncd', 'salmo', 'cocci']
+)
+
+val_generator = val_datagen.flow_from_directory(
+    val_dir,
+    target_size=image_size,
+    batch_size=batch_size,
+    class_mode='categorical',
+    classes=['healthy', 'ncd', 'salmo', 'cocci']
+)
+
+# Create MobileNetV2 base model
+base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+# Freeze the layers of the base model
+for layer in base_model.layers:
+    layer.trainable = False
+
+# Create a new model on top of the MobileNetV2 base
+model = tf.keras.models.Sequential([
+    base_model,
+    tf.keras.layers.GlobalAveragePooling2D(),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dropout(0.35),  # Add dropout with a rate of 0.5 (adjust as needed)
+    tf.keras.layers.Dense(4, activation='softmax')  # Adjust the number of classes as needed
+])
+
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+epochs = 15
+history = model.fit(
+    train_generator,
+    epochs=epochs,
+    validation_data=val_generator
+)
+
+# Save the model
+model.save('mobilenet_model2.1.h5')
+
+
+import matplotlib.pyplot as plt
+
+# Retrieve training history from the model training process
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+# Plot training and validation accuracy
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+
+# Plot training and validation loss
+plt.subplot(1, 2, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
